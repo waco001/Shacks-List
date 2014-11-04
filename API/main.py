5e85d5a13bef54c3c2c08d8b2a52f7f45f73fd3c
waco001 <waco001@Abhisheks-MacBook-Air.local>
2014-11-04 13:29:57 -0500

    Still not working

--- API/main.py
+++ API/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask.ext import restful
-import requests, datetime
+import requests, datetime, logging
 from requests.auth import HTTPBasicAuth
 
 app = Flask(__name__)
@@ -9,17 +9,16 @@ sinchapplicationkey = "678aec4a-15dc-422a-894a-8daaf40dc3cf"
 sinchappsecret = "VQAtORFpzUyt4jP7bb/qtw=="
 class SendSMS(restful.Resource):
     def get(self):
-        url = 'https://messagingApi.sinch.com/v1/sms/+46700000000'
+        url = 'https://messagingApi.Sinch.com/v1/sms/+46700000000'
         payload = {
-        "Message" : "Shaks List Dude!",
-        "From" : "+46000000001"
+        "Message" : "Shacks List Dude!"
         }
         header = {
         "Content-Type": "text/json",
         "X-Timestamp" : str(datetime.datetime.utcnow()).replace(" ","T")+"Z"
         }
-        basic_auth=HTTPBasicAuth('Application' + sinchapplicationkey, sinchapplicationkey)
-        r = requests.post(url, data=payload, headers=header, verify=True,auth=basic_auth)
+        basic_auth=HTTPBasicAuth('application:' + sinchapplicationkey, sinchappsecret)
+        r = requests.post(url, data=payload, headers=header, verify=True,auth=(basic_auth))
         return r.text
 
 api.add_resource(SendSMS, '/')
